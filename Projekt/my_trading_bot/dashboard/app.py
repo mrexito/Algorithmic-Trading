3567667ed26957c26e03de1c6edb377fe5534e80
Reto Furrer <reto.furrer@ost.ch>
2025-10-19 17:57:54 +0200

    Fix errors when running app.py directly

--- Projekt/my_trading_bot/dashboard/app.py
+++ Projekt/my_trading_bot/dashboard/app.py
@@ -1,5 +1,16 @@
+import os
+import sys
+
 import dash
 from dash import dcc, html
+
+# Allow running the script directly (python dashboard/app.py) by making sure the package root is on sys.path.
+if __package__ in (None, ""):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    if parent_dir not in sys.path:
+        sys.path.insert(0, parent_dir)
+
 from dashboard.layouts import tab1_overview_layout, tab2_details_layout, tab3_descriptions_layout
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
