b4b2f7f9f868f3478ed621f1d50f8ab833cdac02
Donat Rüttimann <ruettimann.donat@gmail.com>
2025-10-29 14:12:05 +0100

    test

--- Projekt/my_trading_bot/config/settings.py
+++ Projekt/my_trading_bot/config/settings.py
@@ -1,5 +1,12 @@
 # Enthält globale Projektkonfigurationen
 
-PREDEFINED_SYMBOLS = ["AAPL", "GOOG", "SMI", "NOVARTIS", "NVDA", "TSLA"]
+PREDEFINED_SYMBOLS = [
+    "AAPL",
+    "GOOG",
+    "^SSMI",     
+    "NOVN.SW",    
+    "NVDA",
+    "TSLA",
+]
 CAPITAL = 1000000
 COMMISSION = 0.001
